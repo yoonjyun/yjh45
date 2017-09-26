4dee7fdcf383b9d00527e511972f787b883d20ec
kosta <kosta@192.168.0.131>
2017-09-26 16:24:18 +0900

    멤버VO

--- gitTestY/src/com/kosta/model/MemberVO.java
+++ gitTestY/src/com/kosta/model/MemberVO.java
@@ -1,5 +1,19 @@
 package com.kosta.model;
 
 public class MemberVO {
+	private String id;
 
+	public MemberVO(String id) {
+		super();
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 }
